7e77c7e8c5ee5b7b7427c0db2d78f225d27105a0
nusica <34694136+nusica@users.noreply.github.com>
2017-12-19 23:44:54 +0200

    Test

--- Bank/Bank/BusinessBankAccount.cs
+++ Bank/Bank/BusinessBankAccount.cs
@@ -10,8 +10,12 @@ namespace Bank
     {
         public override void Withdraw(decimal withDrawSum)
         {
+			
+			
             decimal comision= (withDrawSum * 0.01M) / 100;
             AccountSum = withDrawSum - comision;
+			
+			
             Console.WriteLine("Withdraw has been succesfull. Total amount is: {0} and the withdraw sum is {1}", AccountSum, comision);
         }
     }
